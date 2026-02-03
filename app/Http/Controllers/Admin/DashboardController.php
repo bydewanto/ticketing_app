7bb0f6f9966a6ed930bc7066fa5ed9ba17ea3e19
bydewanto <bydewanto@gmail.com>
2026-02-03 23:51:57 +0700

    update dashboard

---
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\ticket;
+use App\Models\events;
+use App\Models\Order;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        $totalEvents = events::count();
+        $totalCategories = \App\Models\Category::count();
+        $totalOrders = Order::count();
+
+        return view('admin.dashboard', compact('totalTickets', 'totalEvents', 'totalOrders'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::prefix('admin')->group(function () {
+        Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    });
+    
 });
 
 require __DIR__.'/auth.php';
